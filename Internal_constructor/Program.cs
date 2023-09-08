de823e8d1e853a0032347315e051c7abe177ab0f
User <User@ANUKESH>
2023-09-08 08:22:36 +0530

    Add project files.

---
+++ Internal_constructor/Program.cs
@@ -0,0 +1,37 @@
+﻿//internal cunstructor class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10question
+{
+    internal class cunstructor
+    {
+        private int x;
+        internal cunstructor(int a)
+        {
+            this.x = a;
+            x++;
+            x++;
+            Console.WriteLine("internal cunstructor :" + x);
+
+        }
+    }
+}
+
+
+
+namespace _10question
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cunstructor inst = new cunstructor(25);
+            Console.ReadKey();
+
+        }
+    }
+}
